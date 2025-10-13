ddd306f31dcf463fe1c98d16969a60058c84a895
fetinin <denka0087@gmail.com>
2025-10-13 23:46:46 +0300

    Add tongi.loca.lt to allowed hosts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  allowedDevOrigins: ['*', 'web.telegram.org', '127.0.0.1'],
+  allowedDevOrigins: ['*', 'web.telegram.org', '127.0.0.1', 'tongi.loca.lt'],
 
   turbopack: {
     root: process.cwd(),
